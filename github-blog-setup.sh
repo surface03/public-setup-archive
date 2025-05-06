e59e638e7417f36fc4ef770ad08744d94255c7e5
surface03 <ginigini03@gmail.com>
2025-05-06 18:44:58 +0900

    add ruby setup

--- github-blog-setup.sh
+++ github-blog-setup.sh
@@ -2,9 +2,13 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install ruby-full build-essential zlib1g-dev
+sudo apt-get install -y software-properties-common build-essential zlib1g-dev
 
-sudo gem install jekyll bundler webrick
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install -y ruby3.1 ruby3.1-dev
+
+sudo gem install bundler jekyll webrick --no-document
 
 bundle config set path 'vendor/bundle'
 
